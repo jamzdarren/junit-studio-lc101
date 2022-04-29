833ed378fea5fb4394fa43d2d043171945bc76b6
James <jamzdarren@gmail.com>
2022-04-28 19:42:10 -0500

    Coded studio tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.junit.Test;
+import main.BalancedBrackets;
 
 import static org.junit.Assert.*;
 
@@ -12,5 +13,65 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    // test 1
+    @Test
+    public void testGoodString1() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets(""), true);
+    }
+    // test 2
+    @Test
+    public void testGoodString2() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"), true);
+    }
+    // test 3
+    @Test
+    public void testGoodString3() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("Launch[Code]"), true);
+    }
+    // test 4
+    @Test
+    public void testGoodString4() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"), true);
+    }
+    // test 5
+    @Test
+    public void testGoodString5() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("[]"), true);
+    }
+    // test 6
+    @Test
+    public void testGoodString6() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("1111[]1111[]1111"), true);
+    }
+    // test 7
+    @Test
+    public void testBadString1() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("[LaunchCode"), false);
+    }
+    // test 8
+    @Test
+    public void testBadString2() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("Launch]Code["), false);
+    }
+    // test 9
+    @Test
+    public void testBadString3() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("["), false);
+    }
+    // test 10
+    @Test
+    public void testBadString4() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("]["), false);
+    }
+    // test 11
+    @Test
+    public void testBadString5() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("111["), false);
+    }
+    // test 12
+    @Test
+    public void testBadString6() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("111[1111["), false);
+    }
 
 }
